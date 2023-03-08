6876c1b2280fe6c3579067036c5d73edcb4a2f41
dinarfathrahmanov <dinfat@mail.ru>
2023-03-08 18:48:32 +0300

    готово

---
+++ src/Author.java
@@ -0,0 +1,17 @@
+public class Author {
+    private String name;
+    private String lastName;
+
+    public Author(String name, String firstName) {
+        this.name = name;
+        this.lastName = firstName;
+    }
+    public String getName() {
+        return this.name;
+    }
+
+
+    public String getLastName() {
+        return this.lastName;
+    }
+}
